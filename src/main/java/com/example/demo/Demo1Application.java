8ddb589756b3bebaf3fc9a3afe82b86b0d6df256
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-09-09 02:23:48 +0100

    test branch created

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,14 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
-        System.out.println("How are you");
+        System.out.println("Live server running");
     }
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+//        Student student1 = new Student("Kelechi", "Divine", "okoroaforkelechi123@gmail.com");
+//        studentRepository.save(student1);
+    }
 }
